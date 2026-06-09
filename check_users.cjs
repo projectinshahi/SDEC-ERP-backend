39dff3fd8787ccea2fb684903ddaf2018f773c69
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2026-06-09 18:07:49 +0530

    update

---
+++ check_users.cjs
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { PrismaClient } = require('@prisma/client');
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const users = await prisma.users.findMany({ select: { id: true, name: true, role: true } });
+  console.log(users);
+}
+main().catch(console.error).finally(() => prisma.$disconnect());
